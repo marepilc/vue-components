baca595e8c0ed7f4cff0fb95062c93a71c1f433b
Marek Pilczuk <user@mp76.pl>
2024-10-12 23:08:04 +0200

    Initial commit

---
+++ components/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        type="button"
+        class="flex items-center justify-around gap-2 rounded bg-primary px-3 py-2 font-semibold text-ink-50 hover:bg-primary-600 focus:outline-none focus:ring-0"
+    >
+        <slot name="icon" />
+        <slot />
+    </button>
+</template>
+
+<style scoped></style>
